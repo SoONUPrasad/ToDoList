88600f7782aa240ee7fec9d367f63d4989da25bb
Sonu <97361485+SoONUPrasad@users.noreply.github.com>
2023-07-28 23:24:10 +0530

    Add files via upload
    Todo

---
+++ index.js
@@ -0,0 +1,23 @@
+const a = 23;
+const b = '23';
+const f1 = ()=>{
+    return "Me";
+}
+console.log(f1());
+function add(a,b){
+    return a+b;
+}
+console.log(add(4,5));
+if(a==b){
+    console.log("equal");
+}else{
+    console.log("not equal");
+}
+const object = {
+    name: "Ankit",
+    age: 24,
+    school: "DHS",
+    Rank: "1st"
+}
+
+console.log(object.Rank)
